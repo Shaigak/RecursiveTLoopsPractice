4cd4f5292acb948e6f6fb9e2bc2ff9b9f8086801
[shaigak] <shaigak@code.edu.az>
2023-02-12 12:41:37 +0300

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -198,77 +198,215 @@
 
 
 
-let user1={
-    id:1,
-    name:"Mirze",
-    surname:"Beshirzade",
-    age:21
-
-}
-
-let user2={
-    id:2,
-    name:"Saiq",
-    surname:"Kazimov",
-    age:23
-
-}
-
-let user3={
-    id:3,
-    name:"Elekber",
-    surname:"Hesenov",
-    age:29
-
-}
-
-let user4={
-    id:4,
-    name:"Mirze",
-    surname:"Beshirzade",
-    age:31
-
-}
-
-let users=[user1,user2,user3,user4];
-
-// function showUsersByAge(people,callback){
-
-// for (const item of people) {
-
-//     if(callback(item.age)){
-//         console.log(item.name +" "+ item.surname)
-//     }  
-// }
+// let user1={
+//     id:1,
+//     name:"Mirze",
+//     surname:"Beshirzade",
+//     age:21
 
 // }
-// showUsersByAge(users,m=>m>25)
+
+// let user2={
+//     id:2,
+//     name:"Saiq",
+//     surname:"Kazimov",
+//     age:23
+
+// }
+
+// let user3={
+//     id:3,
+//     name:"Elekber",
+//     surname:"Hesenov",
+//     age:29
+
+// }
+
+// let user4={
+//     id:4,
+//     name:"Mirze",
+//     surname:"Beshirzade",
+//     age:31
+
+// }
+
+// let users=[user1,user2,user3,user4];
+
+// // function showUsersByAge(people,callback){
+
+// // for (const item of people) {
+
+// //     if(callback(item.age)){
+// //         console.log(item.name +" "+ item.surname)
+// //     }  
+// // }
+
+// // }
+// // showUsersByAge(users,m=>m>25)
 
 
-let filteredDatas=users.filter(m=>m.age>20)
-let sumOfAge=0;
+// let filteredDatas=users.filter(m=>m.age>20)
+// let sumOfAge=0;
 
-for(const item of filteredDatas){
+// for(const item of filteredDatas){
 
-    sumOfAge+=item.age
+//     sumOfAge+=item.age
     
-}
+// }
 
-console.log(sumOfAge)
+// console.log(sumOfAge)
 
 
-const getSumOfAgesFromFilteredDatas=datas=>{
+// const getSumOfAgesFromFilteredDatas=datas=>{
 
-    let sumOfAge=0;
+//     let sumOfAge=0;
 
-for(const item of datas){
+// for(const item of datas){
 
-    sumOfAge+=item.age
+//     sumOfAge+=item.age
     
-}
+// }
 
-console.log(sumOfAge)
+// console.log(sumOfAge)
 
-}
+// }
 
-getSumOfAgesFromFilteredDatas(filteredDatas)
+// getSumOfAgesFromFilteredDatas(filteredDatas)
+
+
+// function SumOfNums(num){
+
+//     if(num==1){
+//         return num;
+//     }else{
+//         return num+SumOfNums(num-1)
+//     }
+// }
+// console.log(SumOfNums(5))
+
+
+
+
+
+
+
+
+
+// Task-1
+// function GetFact(num){
+
+//     if(num==1){
+//         return num;
+//     }else{
+//         return num*GetFact(num-1);
+//     }
+//     }
+
+//     console.log(GetFact(5))
+
+
+// Task-2
+// 3) Icerisinde name, surname, age, email olan bir nece student objectleriniz var.
+//  Ashagida elave etdiklerimi functionlar sekilinde yazin, functionlar objectlerden ibaret array qebul edecek.
+// 1) Yashi 18-25 araliginda olan telebelerin sayi.
+// 2) Emailinde "c" herfi olan studentlerin sayi
+
+
+// let stu1={
+//     name:"Saiq",
+//     surname:"Kazimov",
+//     email:"shaicgak@code.edu.az",
+//     age:25
+
+// }
+
+// let stu2={
+//     name:"Cavid",
+//     surname:"Ismayilzade",
+//     email:"cavidai@code.edu.az",
+//     age:23
+
+// }
+
+// let stu3={
+//     name:"Aqsin",
+//     surname:"Humbetov",
+//     email:"Aq@code.edu.az",
+//     age:21
+
+// }
+
+// let stu4={
+//     name:"Cavid",
+//     surname:"Ismayilzade",
+//     email:"cavidai@code.edu.az",
+//     age:21
+
+// }
+
+// let arry=[stu1,stu2,stu3,stu4];
+
+
+// Task-2
+
+// function GetAge(arr){
+
+//     let count=0;
+
+//     for (const item of arr) {
+
+//         if(item.age>18 && item.age<25){
+//             count++
+//         }
+        
+//     }
+//     console.log(count)
+// }
+
+// GetAge(arry)
+
+
+
+
+// Task3
+
+// function Getword(arr){
+
+//     let count=0;
+
+
+//     for (const item of arr){
+//         if(item.email.includes("c",0)){
+//             // let word = email.contains("c")
+//             // console.log(word)
+//             count++;
+//         }
+//     }
+
+//  console.log(count)
+// }
+
+// Getword(arry)
+
+
+
+
+
+
+
+// Task-4
+// function GetYear(num){
+   
+//     let year=2023;
+
+//     let result=0
+
+//     result=year-num;
+
+//     console.log(result + " " + "Sizin doğum iliniz" )
+// }
+
+// GetYear(50)
+
+
+
